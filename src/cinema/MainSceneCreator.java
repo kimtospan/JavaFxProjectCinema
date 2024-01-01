4e6982e5ce0a9b17461def030862fffeb141f84b
kimtospan <dimitrisvas014@gmail.com>
2024-01-01 19:59:20 +0200

    add fx

---
+++ src/cinema/MainSceneCreator.java
@@ -0,0 +1,25 @@
+package cinema;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+
+public class MainSceneCreator extends SceneCreator implements EventHandler<MouseEvent>{
+    
+    
+    public MainSceneCreator(double width, double height) {
+        
+        }
+    
+    @Override
+    Scene createScene() {
+        
+        }
+    
+    @Override
+    public void handle(MouseEvent event) {
+        
+    }
+}
---
+++ src/cinema/SceneCreator.java
@@ -0,0 +1,14 @@
+package cinema;
+import javafx.scene.Scene;
+
+public abstract class SceneCreator {
+    protected double width;
+    protected double height;
+    
+    public SceneCreator(double width, double height) {
+        this.width = width;
+        this.height = height;
+        }
+    
+    abstract Scene createScene();
+}
