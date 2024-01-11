d0a24ed2d7f397888ff3ad7f3367ea6f8d0e6bde
Kimtospan <dimitrisvas014@gmail.com>
2024-01-11 17:20:25 +0200

    bckbnt

--- src/cinema/PartySceneCreator.java
+++ src/cinema/PartySceneCreator.java
@@ -2,17 +2,25 @@ package cinema;
 
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 public class PartySceneCreator extends SceneCreator implements EventHandler<MouseEvent>{
    GridPane rootGridPane;
+   Button backButton;
 
    
     public PartySceneCreator(double width, double height) {
         super(width, height);
         this.rootGridPane = new GridPane();
         this.rootGridPane.setVgap(10);
+        this.rootGridPane.setHgap(10);
+        this.backButton = new Button("Back");
+
+        this.rootGridPane.add(backButton, 0, 1);
+
+        backButton.setOnMouseClicked(this);
     }
 
     @Override
@@ -28,6 +36,10 @@ public class PartySceneCreator extends SceneCreator implements EventHandler<Mous
         }    
     
         public void handle(MouseEvent event) {
+            if (event.getSource() == backButton) {
+                App.primaryStage.setScene(App.mainScene);
+                App.primaryStage.setTitle("Cinema Management System");
+            }
 
         }
     
